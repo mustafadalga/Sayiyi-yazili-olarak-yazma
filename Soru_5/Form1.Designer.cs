0d63b582532700742b81477f7f216b1fd1af2cac
mustafadalga <25087769+mustafadalga@users.noreply.github.com>
2018-08-13 17:00:15 +0300

    Add files via upload

---
+++ Soru_5/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Soru_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnsonuc = new System.Windows.Forms.Button();
+            this.lblsonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnsonuc
+            // 
+            this.btnsonuc.Location = new System.Drawing.Point(71, 89);
+            this.btnsonuc.Name = "btnsonuc";
+            this.btnsonuc.Size = new System.Drawing.Size(151, 35);
+            this.btnsonuc.TabIndex = 1;
+            this.btnsonuc.Text = "Yazdır";
+            this.btnsonuc.UseVisualStyleBackColor = true;
+            this.btnsonuc.Click += new System.EventHandler(this.btnsonuc_Click);
+            // 
+            // lblsonuc
+            // 
+            this.lblsonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsonuc.Location = new System.Drawing.Point(2, 29);
+            this.lblsonuc.Name = "lblsonuc";
+            this.lblsonuc.Size = new System.Drawing.Size(284, 23);
+            this.lblsonuc.TabIndex = 2;
+            this.lblsonuc.Text = "Sonuç:";
+            this.lblsonuc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblsonuc);
+            this.Controls.Add(this.btnsonuc);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnsonuc;
+        private System.Windows.Forms.Label lblsonuc;
+    }
+}
+
---
+++ Soru_5/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soru_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsonuc_Click(object sender, EventArgs e)
+        {
+            int[] sayibasamaklari = new int[4];
+            int i = 0;
+            string metin = "";
+            string[,] basamaklarstring = { { "", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" }, { "", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" }, { "", "yüz", "iki yüz", "üç yüz", "dört yüz", "beş yüz", "altı yüz", "yedi yüz", "sekiz yüz", "dokuz yüz" }, { "", "bin", "iki bin", "üç bin", "dört bin", "beş bin", "altı bin", "yedi bin", "sekiz bin", "dokuz bin" } };
+            int[] basamaklarint = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int sayi = Convert.ToInt32(textBox1.Text);
+            while (sayi > 0)
+            {
+                sayibasamaklari[i] = sayi % 10;
+                sayi = sayi / 10;
+                i++;
+            }
+            for (i = sayibasamaklari.Length - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (sayibasamaklari[i] == basamaklarint[j])
+                    {
+                        metin += basamaklarstring[i, j] + " ";
+                    }
+                }
+            }
+            lblsonuc.Text = metin;
+        }
+    }
+}
